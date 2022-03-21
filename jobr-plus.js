847e187b8c45c15c310fe4fe41664dc3d02d59fd
David L. Herring <davidlherring@gmail.com>
2022-03-21 17:03:09 -0400

    Setting default quick list codes

--- jobr-plus.js
+++ jobr-plus.js
@@ -1,7 +1,9 @@
 // add your common job codes in here
 var quick_list = [
-    { job: 'FERG006', duration: '0.5', display_name: 'Meeting', task: '422' },
-    { job: 'FERG069', duration: '8.0', display_name: 'Out', task: '422' },
+    { job: 'FERG133', duration: '1', display_name: 'Meeting', task: '453' },
+    { job: 'FERG129', duration: '8.0', display_name: 'Vacation', task: '600' },
+    { job: 'FERG130', duration: '8.0', display_name: 'Sick', task: '601' },
+    { job: 'FERG131', duration: '8.0', display_name: 'Closed', task: '602' },
 ];
 
 function updateTextLinks() {
